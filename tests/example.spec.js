d41e82c1c3a7b0f3826686e7682826163d70598d
omprakash31 <bikisahoo02@gmail.com>
2026-06-28 14:59:29 +0530

    added logs

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,6 +4,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   // Expect a title "to contain" a substring.
+  console.log("loggg");
   await expect(page).toHaveTitle(/Playwright/);
 });
 
