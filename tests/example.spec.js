15dda162a7d932c476e0825ade42056269edecb0
omprakash31 <bikisahoo02@gmail.com>
2026-06-28 16:05:25 +0530

    added logs

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,14 +4,16 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page },testInfo) => {
 
   console.log(testInfo.project.use.baseURL);
-  await page.goto(`${testInfo.project.use.baseURL}`);
+  await page.goto('/');
+  // await page.goto(`${testInfo.project.use.baseURL}`);
   // await page.goto('https://playwright.dev/');
   // Expect a title "to contain" a substring.
   // await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page },testInfo) => {
-  await page.goto(`${testInfo.project.use.baseURL}`);
+  // await page.goto(`${testInfo.project.use.baseURL}`);
+  await page.goto('/');
 
   // Click the get started link.
   // await page.getByRole('link', { name: 'Get started' }).click();
