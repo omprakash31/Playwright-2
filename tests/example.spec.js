2c3e344512edbffc0cfe8ea730104b9130723065
omprakash31 <bikisahoo02@gmail.com>
2026-06-28 14:25:28 +0530

    First commit to main

---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  console.log('test');
+  console.log('log 2');
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
