0c96b6cd4daa6cd54b858adcc169e498bf19f125
omprakash31 <bikisahoo02@gmail.com>
2026-07-25 00:46:52 +0530

    added outputs

--- playwright.config.js
+++ playwright.config.js
@@ -36,14 +36,15 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-  
+  screenshot: 'on',
+  reports:'on',
+  trace: 'on',
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.URL || 'https://playwright.dev/',
+    baseURL: process.env.URL || 'https://playwright.dev/'
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
